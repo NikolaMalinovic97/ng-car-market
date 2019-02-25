4811fbbbca80cffcbfe5e3fae0e45de74feb9d57
Nikola Malinovic <nikolamalinovic97@gmail.com>
2019-02-25 14:56:28 +0100

    Added service called 'cars-data-storage'. This service stores data about cars which are on sale. Also has a function getCarsData(), which returns all cars info.

---
+++ src/app/cars/cars-data-storage.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Car } from '../shared/car.model';
+import { User } from '../shared/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarsDataStorageService {
+  private carsData = [
+    new Car('Golf 2',
+            1982,
+            'diesel',
+            20,
+            1.2,
+            380000,
+            'https://upload.wikimedia.org/wikipedia/commons/e/e8/VW_Golf_II_front_20080206.jpg',
+            'Brz, jak, pouzdan.',
+            300,
+            new User('Aljosa221', 'asda@test.com', '1234567', '066559234')),
+    new Car('VolksWagen',
+            2012,
+            'diesel',
+            150,
+            2.0,
+            120000,
+            'https://s5.pik.ba/galerija/2019-02/01/01/slika-996492-5c543e3fb867f-default.jpg',
+            'Big bold car',
+            12000,
+            new User('Meho', 'asadsda@test.com', 'adagfggas', '066294201')),
+    new Car('Peugeot 407',
+            2006,
+            'diesel',
+            120,
+            1.9,
+            220000,
+            'https://pictures.topspeed.com/IMG/jpg/200612/2006-peugeot-407-40.jpg',
+            'Nice car',
+            5000,
+            new User('Meho', 'asadsda@test.com', 'adagfggas', '066294201'))
+  ];
+
+  constructor() { }
+
+  getCarsData() {
+    return this.carsData.slice();
+  }
+}
--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/shared/car.model';
 import { User } from 'src/app/shared/user.model';
+import { CarsDataStorageService } from '../cars-data-storage.service';
 
 @Component({
   selector: 'app-cars-list',
@@ -8,72 +9,12 @@ import { User } from 'src/app/shared/user.model';
   styleUrls: ['./cars-list.component.css']
 })
 export class CarsListComponent implements OnInit {
-  cars: Car[] = [
-    new Car('Golf 2',
-            1982,
-            'diesel',
-            20,
-            1.2,
-            380000,
-            'https://upload.wikimedia.org/wikipedia/commons/e/e8/VW_Golf_II_front_20080206.jpg',
-            'Brz, jak, pouzdan.',
-            300,
-            new User('Aljosa221', 'asda@test.com', '1234567', '066559234')),
-    new Car('VolksWagen',
-            2012,
-            'diesel',
-            150,
-            3.0,
-            150000,
-            'https://s5.pik.ba/galerija/2019-02/01/01/slika-996492-5c543e3fb867f-default.jpg',
-            'Big bold car',
-            15000,
-            new User('Meho', 'asadsda@test.com', 'adagfggas', '066294201')),
-    new Car('VolksWagen',
-            2012,
-            'diesel',
-            150,
-            3.0,
-            150000,
-            'https://s5.pik.ba/galerija/2019-02/01/01/slika-996492-5c543e3fb867f-default.jpg',
-            'Big bold car',
-            15000,
-            new User('Meho', 'asadsda@test.com', 'adagfggas', '066294201')),
-            new Car('Golf 2',
-            1982,
-            'diesel',
-            20,
-            1.2,
-            380000,
-            'https://upload.wikimedia.org/wikipedia/commons/e/e8/VW_Golf_II_front_20080206.jpg',
-            'Brz, jak, pouzdan',
-            300,
-            new User('Aljosa221', 'asda@test.com', '1234567', '066559234')),
-    new Car('VolksWagen',
-            2012,
-            'diesel',
-            150,
-            3.0,
-            150000,
-            'https://s5.pik.ba/galerija/2019-02/01/01/slika-996492-5c543e3fb867f-default.jpg',
-            'Big bold car',
-            15000,
-            new User('Meho', 'asadsda@test.com', 'adagfggas', '066294201')),
-    new Car('VolksWagen',
-            2012,
-            'diesel',
-            150,
-            3.0,
-            150000,
-            'https://s5.pik.ba/galerija/2019-02/01/01/slika-996492-5c543e3fb867f-default.jpg',
-            'Big bold car',
-            15000,
-            new User('Meho', 'asadsda@test.com', 'adagfggas', '066294201'))
-  ];
+  cars: Car[];
 
-  constructor() { }
+  constructor(private carsDataStorage: CarsDataStorageService) { }
 
   ngOnInit() {
+        this.cars = this.carsDataStorage.getCarsData();
   }
 
 }
